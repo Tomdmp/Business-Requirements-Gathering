85a33aa07fb517df1398206075035d443a274544
Tomdmp <thomas.demalpas-player@cognizant.com>
2025-07-06 17:34:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,34 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 st.title("Business Requirements Chatbot") 
 
-import getpass
-import os
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if not os.environ.get("GOOGLE_API_KEY"):
-  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash") 
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+#llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash") 
 
 
-user_input = st.text_input("Enter a copy of a transcript, email or document:")
+#user_input = st.text_input("Enter a copy of a transcript, email or document:")
 
-if user_input: 
-  response = llm.invoke(user_input)
-  st.write(response.content)
+#if user_input: 
+#  response = llm.invoke(user_input)
+#  st.write(response.content)

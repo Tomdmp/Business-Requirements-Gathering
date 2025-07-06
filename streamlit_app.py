9188fc3defcc70835ce4a60ce1d02f9ba4a2b18c
Tomdmp <thomas.demalpas-player@cognizant.com>
2025-07-06 17:26:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 st.title("Business Requirements Chatbot") 
 
+import getpass
+import os
+
+if not os.environ.get("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash") 
 
 
